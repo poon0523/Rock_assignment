ce753f7c0742938871ed7a5c2b4eb2f31a099848
maiko <urbanrepablic@gmail.com>
2023-09-23 19:10:28 +0900

    test4 push

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,8 +11,8 @@ class OrdersController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      @order = current_user.lock.rders.build(order_params)
-      @order.save
+      @order = current_user.orders.lock.build(order_params)
+      @order.save!
       @order.update_total_quantity
       # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     end
